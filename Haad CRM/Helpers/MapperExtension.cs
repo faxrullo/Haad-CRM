fddf7823e0656e9a3ac5fdcac770f624b8fc7adc
Sayidahror <saidahrormirzaahmedov@gmail.com>
2024-03-03 15:25:38 +0500

    Mappers

---
+++ Haad CRM/Helpers/MapperExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haad_CRM.Helpers;
+
+public static class MapperExtension
+{
+    public static T MapTo<T>(this object obj) where T : class, new()
+    {
+        var objType = obj.GetType();
+        var objProperties = objType.GetProperties();
+
+        var dto = new T();
+        var dtoType = dto.GetType();
+        var dtoProperties = dtoType.GetProperties();
+
+        foreach (var objProperty in objProperties)
+        {
+            if (dtoProperties.Any(p => p.Name == objProperty.Name))
+            {
+                var dtoProperty = dtoType.GetProperty(objProperty.Name);
+                var value = objProperty.GetValue(obj);
+                dtoProperty.SetValue(dto, value);
+            }
+        }
+        return dto;
+    }
+}
